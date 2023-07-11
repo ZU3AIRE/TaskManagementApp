d23ce46742c8941bd871365a98e4064ab797af28
Gullzaibkhan <ngullzaib9@gmail.com>
2023-07-11 15:15:50 +0500

    11JulyTask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UploadComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
